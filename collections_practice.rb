d4baef5326fb7aff44ee3cf071f5731aeeb87df1
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-28 09:38:10 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.size<=>b.size
+  end
+end
+
+
+def swap_elements(array)
+  temp=array[1]
+  array[1]=array[2]
+  array[2]=temp
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
